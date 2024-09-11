1026245ab18f30508afc692590c7170fdb449a7b
Guilherme Galaverna <guigalaverna@gmail.com>
2024-09-10 22:05:40 -0300

    feat: personalizando o portifolio

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
-export const GENERATE_SLUG_FROM_TITLE = true
-export const TRANSITION_API = true
+export const SITE_TITLE = "Guilherme Galaverna | Desenvolvedor Front-end";
+export const SITE_DESCRIPTION =
+  "Sou desenvolvedor front-end que está buscando a primeira experiência comercial.";
+export const GENERATE_SLUG_FROM_TITLE = false;
+export const TRANSITION_API = true;
