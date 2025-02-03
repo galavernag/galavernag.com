61b0ee462a927ced5749910446c390af46f014b8
Guilherme Galaverna <guigalaverna@gmail.com>
2025-02-03 16:48:08 -0300

    feat: create start section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,87 @@
-import Image from "next/image";
+import {
+  FaCloudDownloadAlt,
+  FaGithub,
+  FaLinkedin,
+  FaReact,
+} from "react-icons/fa";
+import { RiNextjsFill } from "react-icons/ri";
+import { SiJavascript, SiTailwindcss, SiTypescript } from "react-icons/si";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main className="max-w-4xl mt-5 md:mx-auto">
+      <header className="flex items-center justify-between">
+        <span className="font-medium">
+          [<span className="mx-0.5 font-normal">GG</span>]
+        </span>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="space-x-3 font-light">
+          <a href="/">INÍCIO</a>
+          <span>//</span>
+          <a href="#sobre-mim">SOBRE-MIM</a>
+          <span>//</span>
+          <a href="#projetos">PROJETOS</a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </header>
+
+      <section className="grid grid-cols-2 items-center gap-12 mt-32">
+        <div className="space-y-6">
+          <h2 className="text-3xl">Olá, sou Guilherme Galaverna</h2>
+          <p className="text-justify block">
+            Bem-vindo ao meu portifólio. Aqui você pode encontrar meus projetos
+            onde pratiquei todo conhecimento de desenvolvimento Front-End com
+            React.js/Next.js com TypeScript, Tailwind
+          </p>
+          <span className="text-sm mt-3">TECNOLOGIAS</span>
+          <ul className="flex items-center gap-2">
+            <li>
+              <FaReact size={24} />
+            </li>
+            <li>
+              <RiNextjsFill size={24} />
+            </li>
+            <li>
+              <SiJavascript size={24} />
+            </li>
+            <li>
+              <SiTypescript size={24} />
+            </li>
+            <li>
+              <SiTailwindcss size={24} />
+            </li>
+          </ul>
+          <footer className="space-x-3 text-black flex">
+            <a
+              href="https://github.com/galavernag.com"
+              className="bg-white p-1.5 rounded-md flex gap-2 font-medium"
+            >
+              <FaGithub color="black" size={20} />
+              GitHub
+            </a>
+
+            <a
+              href="https://www.linkedin.com/in/galavernag/"
+              className="bg-white p-1.5 rounded-md flex gap-2 font-medium"
+            >
+              <FaLinkedin color="black" size={20} />
+              LinkedIn
+            </a>
+
+            <a
+              href=""
+              className="bg-white p-1.5 rounded-md flex gap-2 font-medium"
+            >
+              <FaCloudDownloadAlt color="black" size={20} />
+              CV
+            </a>
+          </footer>
+        </div>
+        <img
+          className="h-auto"
+          src="https://github.com/galavernag.png"
+          alt=""
+        />
+      </section>
+    </main>
   );
 }
