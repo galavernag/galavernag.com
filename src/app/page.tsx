aae1591a73d344a71b5bf88c2a47651485835029
Guilherme Galaverna <guigalaverna@gmail.com>
2025-02-03 18:16:37 -0300

    feat: add placeholder for content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,8 @@ import { RiNextjsFill } from "react-icons/ri";
 import { SiJavascript, SiTailwindcss, SiTypescript } from "react-icons/si";
 
 export default function Home() {
+  const year = new Date().getFullYear();
+
   return (
     <main className="max-w-4xl mt-5 md:mx-auto">
       <header className="flex items-center justify-between">
@@ -18,7 +20,7 @@ export default function Home() {
         <div className="space-x-3 font-light">
           <a href="/">INÍCIO</a>
           <span>//</span>
-          <a href="#sobre-mim">SOBRE-MIM</a>
+          <a href="#sobre-mim">SOBRE_MIM</a>
           <span>//</span>
           <a href="#projetos">PROJETOS</a>
         </div>
@@ -82,6 +84,75 @@ export default function Home() {
           alt=""
         />
       </section>
+
+      <section id="sobre-mim" className="mt-24">
+        <h2 className="text-3xl">#SOBRE_MIM</h2>
+
+        <div className="space-y-6 mt-3">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni,
+            ducimus eum obcaecati quae odit nesciunt temporibus facere totam
+            reprehenderit voluptates numquam molestias. Nesciunt possimus,
+            asperiores quo rerum repudiandae aperiam deleniti?
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni,
+            ducimus eum obcaecati quae odit nesciunt temporibus facere totam
+            reprehenderit voluptates numquam molestias. Nesciunt possimus,
+            asperiores quo rerum repudiandae aperiam deleniti?
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni,
+            ducimus eum obcaecati quae odit nesciunt temporibus facere totam
+            reprehenderit voluptates numquam molestias. Nesciunt possimus,
+            asperiores quo rerum repudiandae aperiam deleniti?
+          </p>
+        </div>
+      </section>
+
+      <section id="projetos" className="mt-24">
+        <h2 className="text-3xl">#PROJETOS</h2>
+
+        <div className="grid grid-cols-3 gap-3 mt-5">
+          <div>
+            <img src="https://placehold.co/300x200/000000/FFFFFF/png" alt="" />
+          </div>
+          <div>
+            <img src="https://placehold.co/300x200/000000/FFFFFF/png" alt="" />
+          </div>
+          <div>
+            <img src="https://placehold.co/300x200/000000/FFFFFF/png" alt="" />
+          </div>
+          <div>
+            <img src="https://placehold.co/300x200/000000/FFFFFF/png" alt="" />
+          </div>
+          <div>
+            <img src="https://placehold.co/300x200/000000/FFFFFF/png" alt="" />
+          </div>
+          <div>
+            <img src="https://placehold.co/300x200/000000/FFFFFF/png" alt="" />
+          </div>
+          <div>
+            <img src="https://placehold.co/300x200/000000/FFFFFF/png" alt="" />
+          </div>
+          <div>
+            <img src="https://placehold.co/300x200/000000/FFFFFF/png" alt="" />
+          </div>
+          <div>
+            <img src="https://placehold.co/300x200/000000/FFFFFF/png" alt="" />
+          </div>
+        </div>
+      </section>
+
+      <footer className="flex items-center justify-center py-12 text-sm text-neutral-800 gap-3">
+        <span>Copyright</span>
+        <span>·</span>
+        <span>{year}</span>
+        <span>·</span>
+        <span>Guilherme Galaverna</span>
+      </footer>
     </main>
   );
 }
