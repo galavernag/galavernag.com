24678a1bc4ff926657d54467fbdc3004bcac959a
Guilherme Galaverna <guigalaverna@gmail.com>
2025-02-20 03:29:14 -0300

    fix: alter to build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
               Hello World ✨
             </h2>
           </div>
-          <h2 className="text-3xl">Hi, I&aposm Guilherme Galaverna</h2>
+          <h2 className="text-3xl">Hi, I'm Guilherme Galaverna</h2>
           <p className="text-justify block">
             Welcome to my portfolio. Here you can find about my personal
             projects where I praticated all of my knowledge in Front-end and
--- src/app/pt-br/page.tsx
+++ src/app/pt-br/page.tsx
@@ -116,9 +116,9 @@ export default function PtBrPage() {
           </h3>
           <p>
             Eu começei a pesquisar sobre desenvolvimento de software quando
-            tinha 12 anos. Minha primeira &quotlinguagem de programação&quot foi
-            Batch Script, no Windows, onde eu escrevia pequenos programas de
-            terminal. Foi nesse período que eu começei a ter mais interesse por
+            tinha 12 anos. Minha primeira "linguagem de programação" foi Batch
+            Script, no Windows, onde eu escrevia pequenos programas de terminal.
+            Foi nesse período que eu começei a ter mais interesse por
             desenvolvimento. Desde então eu venho pesquisando, estudando e
             aplicando cada conhecimento que aprendo dia a após dia
           </p>
